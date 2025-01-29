603693ede5fb2cc28c506b061f2cd24ef14b8600
Smrutik <117387914+smrutikk@users.noreply.github.com>
2025-01-29 10:07:19 +0530

    final

--- back-end/bot.py
+++ back-end/bot.py
@@ -127,4 +127,4 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print(f"Starting the app on port {port}...")
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- back-end/start.sh
+++ back-end/start.sh
@@ -1,3 +1,3 @@
 pip install sentence-transformers
-pip install huggingface-hub
+pip install huggingface_hub==0.17.3
 python bot.py
