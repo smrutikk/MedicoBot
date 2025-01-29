ddeb0372c0e367ea6ebe5ff4afb0835f39220884
Smrutik <117387914+smrutikk@users.noreply.github.com>
2025-01-29 11:57:00 +0530

    Update bot.py

--- back-end/bot.py
+++ back-end/bot.py
@@ -124,7 +124,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    print(f"Starting the app on port {port}...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
